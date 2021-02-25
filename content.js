402aa2b51a0b843c3ae8d52a7e1d83a4239bf19b
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-02-25 22:41:04 +1100

    Trialing blocking function. Managed to hide some elements,

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-alert("You're on Reddit. Get off Reddit")
+// function purge() {
+    // alert("You're on Reddit. Get off Reddit");
+
+    //Removing 'Drafts' Interface
+    try {
+        $("i").replaceWith( " " );
+        console.log($("i:contains(icon icon-upvote)"));
+    } catch(err) {
+        console.log("ERROR BLOCKING CONTENT: " + err);
+    }
+// }
