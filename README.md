84e9b1d6cefa035c6847e3c529043505f0e8000c
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-02-26 14:07:07 +1100

    Clean Up, Submitted to Google Extension Store!

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 ## An extension that prevents you in getting in dumb arguments with bad faith cretins on Reddit.
 
-- Yells at you for navigating to Reddit.
 - Blocks you from Commenting.
 - Hides Upvotes/Downvotes, Reply Button etc
--- content.js
+++ content.js
@@ -1,20 +1,18 @@
-// function purge() {
-    // alert("You're on Reddit. Get off Reddit");
+alert("You're on Reddit. Get off Reddit");
 
-    //Removing 'Drafts' Interface
-    try {
-        let style = document.createElement('style');
-        style.innerHTML = `
-        ._1r4smTyOEZFO91uFIdWW6T.aUM8DQ_Nz5wL0EJc_wte6 {
-        display: none;
-        }
-        ._3KgrO85L1p9wQbgwG27q4y {
-            visibility: hidden;
-        };`
-        let head = document.getElementsByTagName('head')[0];
-        head.appendChild(style);
-
-    } catch(err) {
-        console.log("ERROR BLOCKING CONTENT: " + err);
+//Removing 'Drafts' Interface
+try {
+    let style = document.createElement('style');
+    style.innerHTML = `
+    ._1r4smTyOEZFO91uFIdWW6T.aUM8DQ_Nz5wL0EJc_wte6 {
+    display: none;
     }
-// }
+    ._3KgrO85L1p9wQbgwG27q4y {
+        visibility: hidden;
+    };`
+    let head = document.getElementsByTagName('head')[0];
+    head.appendChild(style);
+
+} catch(err) {
+    console.log("ERROR BLOCKING CONTENT: " + err);
+}
